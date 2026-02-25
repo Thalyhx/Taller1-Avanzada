228cb12903fac20b874ffb2d732b3f8a627baa4e
Sebas <sebassanchezc1@gmail.com>
2026-02-25 10:27:18 -0500

    Agregacion clases ControlVentana, Launcher y modificación ControlPrincipal

--- src/edu/udistrital/taller1/control/ControlVentana.java
+++ src/edu/udistrital/taller1/control/ControlVentana.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package edu.udistrital.taller1.control;
 
@@ -8,8 +8,6 @@ package edu.udistrital.taller1.control;
  *
  * @author sebas
  */
-public class ControlPrincipal {
-
-    
+public class ControlVentana {
     
 }
