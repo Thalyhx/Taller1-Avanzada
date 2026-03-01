09d1a7e7921b13f777e72820e18a05efcfae1a2d
Nath <nathalycis20@gmail.com>
2026-02-28 22:36:35 -0500

    Cambiando de carpeta al archivo launcher

--- src/edu/udistrital/taller1/control/Launcher.java
+++ src/edu/udistrital/taller1/control/Launcher.java
@@ -6,7 +6,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package edu.udistrital.taller1;
+package edu.udistrital.taller1.control;
 
 import edu.udistrital.taller1.vista.VentanaPrincipal;
 import edu.udistrital.taller1.control.ControlPrincipal;
